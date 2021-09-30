ca28227a60fa132cf24c737cfeeedec5be2063a2
Jonathan Corral <johnlejardinnier@gmail.com>
2021-09-30 15:35:51 +0200

    customise conf

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '891a4f3ca8bb49b1917dfa0e587f1225',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Next Notion Template',
+  domain: 'notion-template.com',
+  author: 'Next Notion',
 
   // open graph metadata (optional)
   description: 'Example site description',
